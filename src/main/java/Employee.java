85108ac1f6916a30142d9054ee8a38db68bdc4a7
Alexander <la_garage@ukr.net>
2022-12-18 16:34:15 +0200

    added some classes(Car, Employee, SameName) and packages

---
+++ src/main/java/Employee.java
@@ -0,0 +1,56 @@
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private long phone;
+    private int age;
+
+    public Employee(String fullName, String position, String email, long phone, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
